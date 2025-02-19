e4a8e9051a1e1e4a892d55fca5074f21d5b9bb71
qmaximp <soldatkin.maks2018@yandex.ru>
2025-02-19 16:16:08 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 			</head>
 			<body className={nunito.variable}>
 				<Providers>{children}</Providers>
+				<div>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</div>
 			</body>
 		</html>
 	)
