02d6fdb267684ea75d8688981976baec10a3aa29
qmaximp <soldatkin.maks2018@yandex.ru>
2025-02-19 15:59:52 +0300

    update

--- src/components/shared/AdressInput.tsx
+++ src/components/shared/AdressInput.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { AddressSuggestions } from 'react-dadata'
+
 import 'react-dadata/dist/react-dadata.css'
 interface Props {
 	onChange?: (value?: string) => void
@@ -13,12 +13,13 @@ export const AdressInput = ({ onChange }: Props) => {
 	return (
 		<>
 			{isClient ? (
-				<AddressSuggestions
-					token='1914854afcecce241bef1af5157a2694eda9a351'
-					onChange={(data: { value: string | undefined }) =>
-						onChange?.(data?.value)
-					}
-				/>
+				// <AddressSuggestions
+				// 	token='1914854afcecce241bef1af5157a2694eda9a351'
+				// 	onChange={(data: { value: string | undefined }) =>
+				// 		onChange?.(data?.value)
+				// 	}
+				// />
+				<input type='text' />
 			) : null}
 		</>
 	)
